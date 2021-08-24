3c968460a6629a638bd9fcdf4b1fb9f29903ab2d
syed747 <86574304+syed747@users.noreply.github.com>
2021-08-24 10:47:22 +0300

    Add files via upload

---
+++ Knightingale/JavaBasics.java
@@ -0,0 +1,21 @@
+package Knightingale;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		//Data declaration....
+				int i=100;
+				int j=200;
+				
+				//Processing of Data...
+				
+				int sum=i+j;
+				
+				//output
+				
+				System.out.println("The result is...:"+sum);
+				
+			}
+}
