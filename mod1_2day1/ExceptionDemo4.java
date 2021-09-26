bf634b76344539549213ada62b845ece4c1ab690
syed747 <86574304+syed747@users.noreply.github.com>
2021-09-26 13:19:13 +0300

    Add files via upload

---
+++ mod1_2day1/ExceptionDemo4.java
@@ -0,0 +1,34 @@
+package mod1_2day1;
+
+public class ExceptionDemo4 {
+	public static void main(String[] args) {
+		ATM canaraATM=new ATM();
+		try {
+			canaraATM.withDrawMoney(2000);
+			System.out.println("swaha.............");
+		}catch(MaxLimitForWithDrawException e) {
+			System.out.println(e);
+			System.out.println("Please enter a value lesser than 2000....");
+		}
+	}
+}
+class ATM{
+	public void withDrawMoney(int amt) throws MaxLimitForWithDrawException{
+		if(amt>=2000) {
+			throw new MaxLimitForWithDrawException("Amount is greater than or equal to 2000....");
+		}
+	}
+}
+//custom exception class
+class MaxLimitForWithDrawException extends Exception{
+	String msg;
+	public MaxLimitForWithDrawException(String msg) {
+		this.msg=msg;
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Exception is........."+this.msg;
+	}
+
+}
